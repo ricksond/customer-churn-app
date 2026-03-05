0fd4a1aa6050b4a66e93f6441f322249260278e5
Rickson Dsouza <ricksond2003@gmail.com>
2026-03-04 19:35:24 -0500

    Data Loaded and preprocessed,Ready for training

---
+++ backend/pipeline/preprocess.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import os
+
+def load_df():
+    base_path=os.path.dirname(os.path.abspath(__file__)) 
+    csv_path=os.path.join(base_path,"../../data/churn_features.csv")
+
+# Normalize csv path
+    csv_path=os.path.normpath(csv_path)
+
+    df=pd.read_csv(csv_path)
+
+    # ML Models need numeric values so change boolean values in "senior_citizen" and "churn"
+    df["churn"]=df["churn"].map({'t':1,'f':0})
+    df["senior_citizen"]=df["senior_citizen"].map({'t':1,'f':0})
+    print(df.head())
+    return df
+
+load_df()
